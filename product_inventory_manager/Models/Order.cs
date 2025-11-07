813f25c22c835d87ef89ce10bd12b403c6034548
JohnIanOngayi <ongayijohnian@gmail.com>
2025-11-07 08:35:53 +0300

    Add project files.

---
+++ product_inventory_manager/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace product_inventory_manager.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ product_inventory_manager/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace product_inventory_manager.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; } // In inventory
+        public float Price { get; set; }
+    }
+}
